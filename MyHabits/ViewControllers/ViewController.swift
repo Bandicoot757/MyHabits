b8f45aeffbc5456f4846f1196739a1d212cc910b
Stanislav Leontyev <arbuspilot@gmail.com>
2020-11-20 00:18:29 +0400

    Initial commit

---
+++ MyHabits/ViewControllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Stanislav Leontyev on 03.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
